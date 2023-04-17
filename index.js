34ad7ac09605bccb30f9f855eaa6c510f7de623e
sarfu786 <sarfarazmohammed768@gmail.com>
2023-04-17 12:26:09 +0530

    all files uploaded

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = "sarfaraz";
+console.log(a);
+var b = 23
+console.log(b)
+var c;
+console.log(c)
+var d=null
+console.log(d)
+console.log("sarfu"-"23")
+console.log(isNaN(20-5))
+let obj = {
+    nam : "sarfu",
+    age:"23",
+    city:"banglore"
+
+}
+console.log(obj.age);
+obj.age = "28"
+console.log(obj.age);
+let ab = " sarfaRAaz"
+console.log(ab.toLowerCase());
+console.log(ab.split(),ab);
+console.log(ab.slice(3));
+console.log(ab.replace('sarfaRAaz','ryuisarfaRAaz'));
+console.log(ab.substring(0,7));
+
+
+for(let i=1;i<=10;i++){
+    console.log(`3*${i} = ${i*3}`);
+}
+console.log("");
+
+for(let i=1;i<=10;i++){
+    console.log(`5*${i}=${i*3}`)
+}
+
